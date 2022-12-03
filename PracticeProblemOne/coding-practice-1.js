595a65f506d1f78499843f3fa473da4cf32a4587
Victoria <veerod0804@yahoo.com>
2022-12-03 15:46:10 -0600

    Completed coding exercise 1

---
+++ PracticeProblemOne/coding-practice-1.js
@@ -0,0 +1,10 @@
+let triangleBase;
+let triangleHeight;
+let triangleArea;
+
+function findArea(triangleBase, triangleHeight){
+triangleArea = (triangleBase * triangleHeight) / 2;
+return triangleArea;
+}
+
+console.log(findArea(10, 10));
